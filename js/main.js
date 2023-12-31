de690bfc5978167ba2d0be51e47cd151ad963192
Yunzhi Li <68145642+Melissayunzhi@users.noreply.github.com>
2023-12-31 18:09:28 +0000

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/aTLvv7yTzY3-beEZ/scene.splinecode');
