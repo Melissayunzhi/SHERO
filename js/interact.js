92259f3e9e062147a0590889d39f97010003c616
Yunzhi Li <melissaliyunzhi@gmail.com>
2024-01-13 21:36:49 +0000

    add static image at the start

--- js/interact.js
+++ js/interact.js
@@ -28,31 +28,34 @@ const enableCam = async () => {
 
 
 const predictWebcam = async () => {
-    const video = document.getElementById("webcam");
-    const canvasElement = document.getElementById("output_canvas");
-    const canvasCtx = canvasElement.getContext("2d");
-    const gestureOutput = document.getElementById("gesture_output");
-  
-    const results = await gestureRecognizer.recognizeForVideo(video, Date.now());
-    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-  
-    if (results.gestures.length > 0) {
+  const video = document.getElementById("webcam");
+  const canvasElement = document.getElementById("output_canvas");
+  const canvasCtx = canvasElement.getContext("2d");
+  const gestureOutput = document.getElementById("gesture_output");
+  const stillImage = document.getElementById("stillImage");
+  const animatedImage = document.getElementById("animatedImage");
+
+  const results = await gestureRecognizer.recognizeForVideo(video, Date.now());
+  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+
+  if (results.gestures.length > 0) {
       const categoryName = results.gestures[0][0].categoryName;
       const categoryScore = parseFloat(results.gestures[0][0].score * 100).toFixed(2);
-  
+
       gestureOutput.innerText = `Gesture: ${categoryName}, Confidence: ${categoryScore}%`;
       gestureOutput.style.display = "block";
-  
-    // Check for the 'ILoveYou' gesture instead of 'Victory'
-    if (categoryName === 'ILoveYou') {
-      document.getElementById('victoryImage').style.display = 'block'; // Consider renaming 'victoryImage' to something more generic like 'gestureImage'
-    } else {
-      document.getElementById('victoryImage').style.display = 'none';
-    }
+
+      if (categoryName === 'ILoveYou') { // Change the gesture name as per your requirement
+        stillImage.style.display = 'none';
+          animatedImage.style.display = 'block';
+      } else {
+          animatedImage.style.display = 'none';
+          stillImage.style.display = 'block';
+      }
   } else {
-    gestureOutput.style.display = "none";
+      gestureOutput.style.display = "none";
   }
   window.requestAnimationFrame(predictWebcam);
 };
-  
-  createGestureRecognizer();
+
+createGestureRecognizer();
